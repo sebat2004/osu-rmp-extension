fca2b2bc4a8fb576f4e2400ed35e0a82d9fb69d7
Seba <sebastiantorresola3@gmail.com>
2023-08-04 16:42:42 -0700

    Fixed duplicate lines issue

--- src/content.js
+++ src/content.js
@@ -1,21 +1,23 @@
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
 // Callback function executes with any change to DOM
-var observer = new MutationObserver(function() {
+var observer = new MutationObserver(function(mutations) {
     const instructorDiv = document.querySelector("div.instructor-detail");
     let changedDiv = false;
-    if (instructorDiv && !changedDiv) {
-        changedDiv = true;
-        const instructorName = instructorDiv.innerHTML;
-        getInstructorRating(instructorName).then((response) => {
-            // Prevents multiple ratings from being added
-            if (response.ratingStats && !instructorDiv.innerHTML.includes("<br>")) {
-                instructorDiv.innerHTML = `${instructorName} <a target="_blank" rel="noopener noreferrer" style='text-decoration:none;' href='https://www.ratemyprofessors.com/professor/${response.ratingStats.legacyId}'>(Link to RMP page)</a>` + `<div style='display:flex;gap:3px;'><h4 style='color:blue;font-weight:500;'>Rate My Professors Rating: </h4><h4>${response.ratingStats.avgRating}/5</h4></div>`;
-            } else if (!instructorDiv.innerHTML.includes("<br>")) {
-                instructorDiv.innerHTML = `${instructorName} <a target="_blank" rel="noopener noreferrer" style='text-decoration:none;' href='https://www.ratemyprofessors.com/search/professors/742?q=${instructorName}'>(Link to RMP search results)</a>` + "<div style='display:flex;gap:4px;'><h4 style='color:blue;font-weight:500;'>Rate My Professor Rating: </h4><h4>No Ratings Yet</h4></div>";
-            }
-        });
-    }
+    mutations.forEach(function(mutation) {
+        if (instructorDiv && !changedDiv) {
+            changedDiv = true;
+            const instructorName = instructorDiv.innerHTML;
+            getInstructorRating(instructorName).then((response) => {
+                // Prevents multiple ratings from being added
+                if (response.ratingStats && !instructorDiv.innerHTML.includes("</a>")) {
+                    instructorDiv.innerHTML = `${instructorName} <a target="_blank" rel="noopener noreferrer" style='text-decoration:none;' href='https://www.ratemyprofessors.com/professor/${response.ratingStats.legacyId}'>(Link to RMP page)</a>` + `<div style='display:flex;gap:3px;'><h5 style='color:blue;font-weight:500;'>Rate My Professors Rating: </h5><h5>${response.ratingStats.avgRating}/5</h5></div>`;
+                } else if (!instructorDiv.innerHTML.includes("</a>")) {
+                    instructorDiv.innerHTML = `${instructorName} <a target="_blank" rel="noopener noreferrer" style='text-decoration:none;' href='https://www.ratemyprofessors.com/search/professors/742?q=${instructorName}'>(Link to RMP search results)</a>` + "<div style='display:flex;gap:4px;'><h5 style='color:blue;font-weight:500;'>Rate My Professor Rating: </h5><h5>No Ratings Yet</h5></div>";
+                }
+            });
+        }
+    });
 });
 
 // Start observing DOM
