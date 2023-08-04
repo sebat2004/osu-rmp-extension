95a4d2c31e27f915d8b019f65c8973a9a29d1baf
Seba <sebastiantorresola3@gmail.com>
2023-08-03 23:22:20 -0700

    Added styles and links to professor pages

--- src/background.js
+++ src/background.js
@@ -1,12 +1,15 @@
 const ratings = require('@mtucourses/rate-my-professors').default;
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    ratings.searchSchool("Oregon State University").then((schools) => {
-        console.log(request.instructorName, schools[0].id)
-        ratings.searchTeacher(request.instructorName, schools[0].id).then((teachers) => {
-            ratings.getTeacher(teachers[0].id).then((ratingStats) => {
-                sendResponse({ratingStats: ratingStats});
-            });
+    // Gets teacher id from teacher name and school id
+    ratings.searchTeacher(request.instructorName, "U2Nob29sLTc0Mg==").then((teachers) => {
+        if (teachers.length == 0) {
+            sendResponse({ratingStats: null});
+            return;
+        }
+        // Gets rating stats from teacher id
+        ratings.getTeacher(teachers[0].id).then((ratingStats) => {
+            sendResponse({ratingStats: ratingStats});
         });
     });
     return true;
--- src/content.js
+++ src/content.js
@@ -1,29 +1,30 @@
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
-// Called with any change to DOM
-var observer = new MutationObserver(function(mutations, observer) {
+// Callback function executes with any change to DOM
+var observer = new MutationObserver(function() {
     const instructorDiv = document.querySelector("div.instructor-detail");
     let changedDiv = false;
     if (instructorDiv && !changedDiv) {
         changedDiv = true;
         const instructorName = instructorDiv.innerHTML;
         getInstructorRating(instructorName).then((response) => {
-            if (response && !instructorDiv.innerHTML.includes("<br>")) {
-                instructorDiv.innerHTML = instructorDiv.innerHTML + "<br>" + response.ratingStats.avgRating;
-                console.log("Rating change successful")
-            } else {
-                console.log("Rating change unsuccessful");
+            // Prevents multiple ratings from being added
+            if (response.ratingStats && !instructorDiv.innerHTML.includes("<br>")) {
+                instructorDiv.innerHTML = `${instructorName} <a target="_blank" rel="noopener noreferrer" style='text-decoration:none;' href='https://www.ratemyprofessors.com/professor/${response.ratingStats.legacyId}'>(Link to RMP page)</a>` + `<div style='display:flex;gap:3px;'><h4 style='color:blue;font-weight:500;'>Rate My Professors Rating: </h4><h4>${response.ratingStats.avgRating}/5</h4></div>`;
+            } else if (!instructorDiv.innerHTML.includes("<br>")) {
+                instructorDiv.innerHTML = `${instructorName} <a target="_blank" rel="noopener noreferrer" style='text-decoration:none;' href='https://www.ratemyprofessors.com/search/professors/742?q=${instructorName}'>(Link to RMP search results)</a>` + "<div style='display:flex;gap:4px;'><h4 style='color:blue;font-weight:500;'>Rate My Professor Rating: </h4><h4>No Ratings Yet</h4></div>";
             }
         });
     }
 });
 
+// Start observing DOM
 observer.observe(document, {
     subtree: true,
-    attributes: true,
-    childList: false
+    attributes: true
 });
 
+// Communicates with background.js to get instructor rating from Rate My Professors
 const getInstructorRating = async (instructorName) => {
     const rating = await chrome.runtime.sendMessage({
         instructorName: instructorName,
