119d27b3fcf54ef667a891d4471db733ecdf7f23
Seba <sebastiantorresola3@gmail.com>
2023-08-12 13:49:46 -0700

    Added school search request

--- src/background.js
+++ src/background.js
@@ -1,15 +1,22 @@
 const ratings = require('@mtucourses/rate-my-professors').default;
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    // Gets teacher id from teacher name and school id
-    ratings.searchTeacher(request.instructorName, "U2Nob29sLTc0Mg==").then((teachers) => {
-        if (teachers.length == 0) {
+    // Gets school id from school name
+    ratings.searchSchool(request.schoolName).then((schools) => {
+        if (schools.length == 0) {
             sendResponse({ratingStats: null});
             return;
         }
-        // Gets rating stats from teacher id
-        ratings.getTeacher(teachers[0].id).then((ratingStats) => {
-            sendResponse({ratingStats: ratingStats});
+        // Gets teacher id from teacher name and school id
+        ratings.searchTeacher(request.instructorName, schools[0].id).then((teachers) => {
+            if (teachers.length == 0) {
+                sendResponse({ratingStats: null});
+                return;
+            }
+            // Gets rating stats from teacher id
+            ratings.getTeacher(teachers[0].id).then((ratingStats) => {
+                sendResponse({ratingStats: ratingStats});
+            });
         });
     });
     return true;
