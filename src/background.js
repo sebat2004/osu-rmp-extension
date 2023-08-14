8306d06dbf7e60107e33fb05aaeef65959c93a2a
sebat2004 <sebastiantorresola3@gmail.com>
2023-08-13 23:34:33 -0700

    Added RMP API format in comment

--- src/background.js
+++ src/background.js
@@ -3,19 +3,33 @@ const ratings = require('@mtucourses/rate-my-professors').default;
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     // Gets school id from school name
     ratings.searchSchool(request.schoolName).then((schools) => {
-        if (schools.length == 0) {
+        if (schools.length === 0) {
             sendResponse({ratingStats: null});
             return;
         }
         // Gets teacher id from teacher name and school id
         ratings.searchTeacher(request.instructorName, schools[0].id).then((teachers) => {
-            if (teachers.length == 0) {
+            if (teachers.length === 0) {
                 sendResponse({ratingStats: null});
                 return;
             }
             // Gets rating stats from teacher id
             ratings.getTeacher(teachers[0].id).then((ratingStats) => {
                 sendResponse({ratingStats: ratingStats});
+                {/*
+                    API FORMAT:
+                    
+                    avgDifficulty : 0
+                    avgRating : 0
+                    department : "Information Science"
+                    firstName : "Yang"
+                    id : "VGVhY2hlci0yNjIxODUw"
+                    lastName : "Wang"
+                    legacyId : "2621850"
+                    numRatings : 0
+                    school : {}
+                    wouldTakeAgainPercent : -1
+                */}
             });
         });
     });
