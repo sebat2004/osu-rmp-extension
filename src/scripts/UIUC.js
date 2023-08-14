b549bfcf9635f5c1093694df680f3efb129b24d5
sebat2004 <sebastiantorresola3@gmail.com>
2023-08-14 16:09:27 -0700

    Finished UIUC styling and link to RMP page

--- src/scripts/UIUC.js
+++ src/scripts/UIUC.js
@@ -5,7 +5,8 @@ let instructorNames = new Map()
 
 // Finds instructor name
 for (const instructorRow of document.querySelectorAll("td.instructor")) {
-    instructorDiv = instructorRow.firstElementChild
+    let instructorDiv = instructorRow.firstElementChild
+    let newDiv = document.createElement("div")
 
     let sibling = instructorDiv.firstChild
     while (sibling) {
@@ -13,15 +14,19 @@ for (const instructorRow of document.querySelectorAll("td.instructor")) {
             sibling = sibling.nextSibling
             continue
         }
+        let instructorName = sibling.nodeValue
+        
+
         if (!instructorNames.has(sibling)) {
-            let newDiv = instructorDiv;
             getInstructorRating(sibling.nodeValue, "University Of Illinois at Urbana").then((response) => {
                 if (response.ratingStats) {
                     instructorNames.set(sibling, response.ratingStats)
-                    newDiv.after(` ${response.ratingStats.avgRating}/5`)
+                    newDiv.innerHTML += `<div style='display:flex;gap:4px;'><a target="_blank" rel="noopener noreferrer" style='display:flex;' href='https://www.ratemyprofessors.com/professor/${response.ratingStats.legacyId}'>${instructorName}</a><h7 style='padding-left:8px;color:blue;font-weight:500;'>Rating:</h7><h7>${response.ratingStats.avgRating}/5</h7></div>`
+                    instructorDiv.innerHTML = newDiv.innerHTML
                 } else {
                     instructorNames.set(sibling, null)
-                    newDiv.after(" No Ratings Yet")
+                    newDiv.innerHTML += `<div style="display:flex;gap:4px;"><a target="_blank" rel="noopener noreferrer" style='display:flex;' href='https://www.ratemyprofessors.com/search/professors/1112?q=${instructorName}'>${instructorName}</a><h7 style='padding-left:8px;color:blue;font-weight:500;'>Rating:</h7><h7>N/A</h7></div>`
+                    instructorDiv.innerHTML = newDiv.innerHTML
                 }});
         } else if (instructorNames.has(sibling) && instructorNames.get(sibling) !== null) {
             instructorDiv.after(instructorNames.get(sibling).rating)
