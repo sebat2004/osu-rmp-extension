ab03468e8316a82d41a41f954527e69a77b302cc
Seba <sebastiantorresola3@gmail.com>
2023-08-30 17:24:07 -0700

    Added middle name trimming functionality to get better results

--- src/scripts/OhioSU.js
+++ src/scripts/OhioSU.js
@@ -1,16 +1,20 @@
-import { getInstructorRating } from '../utils'
+import { getInstructorRating, trimMiddleName} from '../utils'
 
 // Instructor div: li.right ng-binding ng-scope
 setTimeout(() => {
     const instructors = document.querySelectorAll('li.right.ng-binding.ng-scope')
     instructors.forEach(async (instructorDiv) => {
-        let instructorName = instructorDiv.lastChild.nodeValue
-        await getInstructorRating(instructorName, "The Ohio State University").then((response) => {
-            if (response.ratingStats) {
+        let instructorName = instructorDiv.lastChild.nodeValue.trim()
+
+        // Trims middle name if it exists
+        instructorName = trimMiddleName(instructorName)
+                
+        await getInstructorRating(instructorName, "The Ohio State University", "U2Nob29sLTcyNA==").then((response) => {
+            if (response.ratingStats && response.ratingStats.avgRating !== 0) {
                 instructorDiv.innerHTML = `<div style='display:flex;gap:4px;'><a target="_blank" rel="noopener noreferrer" style='display:flex;' href='https://www.ratemyprofessors.com/professor/${response.ratingStats.legacyId}'>${instructorName}</a><h7 style='padding-left:8px;color:blue;font-weight:500;'>Rating:</h7><h7>${response.ratingStats.avgRating}/5</h7></div>`
             } else {
                 instructorDiv.innerHTML = `<div style="display:flex;gap:4px;"><a target="_blank" rel="noopener noreferrer" style='display:flex;' href='https://www.ratemyprofessors.com/search/professors/724?q=${instructorName}'>${instructorName}</a><h7 style='padding-left:8px;color:blue;font-weight:500;'>Rating:</h7><h7>N/A</h7></div>`
             }
         });
     });
-}, 6000)
+}, 2000)
--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,21 @@
 // Communicates with background.js to get instructor rating from Rate My Professors
-export const getInstructorRating = async (instructorName, schoolName) => {
+export const getInstructorRating = async (instructorName, schoolName, schoolId) => {
     const rating = await chrome.runtime.sendMessage({
         instructorName: instructorName,
-        schoolName: schoolName
+        schoolName: schoolName,
+        schoolId: schoolId
     }).then((rating) => {
         return rating;
     });
     return rating;
+}
+
+// Trims middle name from full name
+export const trimMiddleName = (fullName) => {
+    const nameArray = fullName.split(" ");
+    if (nameArray.length === 3) {
+        return `${nameArray[0]} ${nameArray[2]}`;
+    } else {
+        return fullName;
+    }
 }
